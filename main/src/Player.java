30c3cd44f2c2cb57992e0041de2085c68a99198f
ClaytonWhitten <whittenc@purdue.edu>
2022-10-05 22:17:24 -0400

    add player class

---
+++ main/src/Player.java
@@ -0,0 +1,14 @@
+// import statements
+/**
+ * An object class for creating players for the ChessGame program
+ *
+ * ChessGame Project
+ *
+ * @author Clayton Whitten & Karan Ravindra
+ * @version Oct 5, 2022
+ */
+
+public class Player {
+
+
+}
